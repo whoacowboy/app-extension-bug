fe813e5f3b4849442fc29d17e23b435ef6ca513a
James Butler <james@whoacowboy.com>
2024-12-22 15:16:47 -0500

    Simplifying vue component

--- ui/src/components/Component.vue
+++ ui/src/components/Component.vue
@@ -1,10 +1,5 @@
 <template>
-  <q-btn
-    class="component"
-    :data-item="item"
-    :label="item"
-    @click="action"
-  ></q-btn>
+  <q-btn class="component" :label="item" @click="action"></q-btn>
 </template>
 <script setup>
 import { ref } from 'vue'
@@ -12,7 +7,3 @@ import { ref } from 'vue'
 const item = ref('hello')
 const action = () => (item.value = Math.random().toString(36).substring(2, 12))
 </script>
-<style lang="scss">
-.component {
-}
-</style>
