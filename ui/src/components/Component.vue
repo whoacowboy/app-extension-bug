7358b003a200aa83ad6830785067c3899fc04dcb
James Butler <james@whoacowboy.com>
2024-12-22 14:49:11 -0500

    second initial commit

---
+++ ui/src/components/Component.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-btn
+    class="component"
+    :data-item="item"
+    :label="item"
+    @click="action"
+  ></q-btn>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const item = ref('hello')
+const action = () => (item.value = Math.random().toString(36).substring(2, 12))
+</script>
+<style lang="scss">
+.component {
+}
+</style>
